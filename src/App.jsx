d52c561e6e8f952cdf9e6f3a2ec6e0ebfa22b3b6
Bijay <bijaymahato031@gmail.com>
2024-04-12 18:30:03 +0545

    clicker project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Cookies from "./assets/cookies.jpeg";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleclick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.counter} </h1>
+        <div className="CookieWrapper">
+          <img onClick={this.handleclick} src={Cookies} />
+        </div>
+        {/* <button onClick={this.handleclick}>Click Garrnus!</button> */}
+      </div>
+    );
+  }
+}
+export default App;
